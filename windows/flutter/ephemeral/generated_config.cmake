c6d2898dc8791c881ed2f0dbfe748df6ec875072
ahadi818 <106049641+ahadi818@users.noreply.github.com>
2025-04-16 11:44:05 +0600

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\UseR\\OneDrive\\Desktop\\MyApp\\e_parking_challan" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Flutter"
+  "PROJECT_DIR=C:\\Users\\UseR\\OneDrive\\Desktop\\MyApp\\e_parking_challan"
+  "FLUTTER_ROOT=C:\\Flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\UseR\\OneDrive\\Desktop\\MyApp\\e_parking_challan\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\UseR\\OneDrive\\Desktop\\MyApp\\e_parking_challan"
+  "FLUTTER_TARGET=C:\\Users\\UseR\\OneDrive\\Desktop\\MyApp\\e_parking_challan\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\UseR\\OneDrive\\Desktop\\MyApp\\e_parking_challan\\.dart_tool\\package_config.json"
+)
